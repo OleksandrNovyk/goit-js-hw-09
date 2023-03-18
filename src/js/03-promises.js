164055f0846ecd2d35a736b4adb799702e061d27
OleksandrNovyk <novykav@gmail.com>
2023-03-18 19:12:58 +0200

    src/js/03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -28,15 +28,16 @@ function createPromise(position, delay) {
 
 function onSubmitForm(event) {
   event.preventDefault();
-  const { delay, step, amount } = event.currentTarget.elements;
+  const { delay, step, amount } = event.currentTarget.elements;          //delay=150,step=100,amount=6
   if (delay.value < 0 || step.value < 0 || amount.value < 0) {
     Notiflix.Notify.warning(`❗ Please enter a positive number`);
   } else {
     for (let i = 0; i < amount.value; i++) {
       let position = i + 1;
-      const delays = Number(delay.value) + step.value * i;
+      let promiseDelay = Number(delay.value) + Number(step.value) * i;   //150+100*0=150, 150+100*1= 250, 150+100*2=350, 150+100*3=450, 150+100*4=550, 150+100*5=650 
+      console.log(promiseDelay);
 
-      createPromise(position, delays)
+      createPromise(position, promiseDelay)
         .then(({ position, delay }) => {
           Notiflix.Notify.success(
             `✅ Fulfilled promise ${position} in ${delay}ms`, options
